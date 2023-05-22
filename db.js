a0e066ab10ea888cbe7ee8b42d6d8545e0b74361
Tayla Nobbs <63324253+tayla-nobbs@users.noreply.github.com>
2023-05-22 16:44:10 +1200

    shows todos filter by non completed

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const config = require('./knexfile').development
 const db = require('knex')(config)
 
 function getTodos() {
-  return db('todos').select()
+  return db('todos').where('complete', false)
 }
 
 function getTaskById(id) {
