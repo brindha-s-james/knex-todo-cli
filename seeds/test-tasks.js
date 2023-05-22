23d089ecc7b68efe1deef1f6560c2fa821bc3132
amanda <63027140+amanda-yvette-turnbull@users.noreply.github.com>
2023-05-22 16:13:53 +1200

    add complteted

--- seeds/test-tasks.js
+++ seeds/test-tasks.js
@@ -6,8 +6,8 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('todos').del()
   await knex('todos').insert([
-    {id: 1, name: 'vaccuming'},
-    {id: 2, name: 'laundry'},
-    {id: 3, name: 'groceries'}
+    {id: 1, name: 'vaccuming', completed: false},
+    {id: 2, name: 'laundry', completed: false},
+    {id: 3, name: 'groceries', completed: false}
   ]);
 };
